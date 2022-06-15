34564d50bc81342feda268f5b39b4ff182620326
Brendan Allan <brendonovich@outlook.com>
2022-06-16 00:44:35 +0800

    initial setup

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,75 @@
+import { createMemo, createSignal, onCleanup } from "solid-js";
+
+function getWindow() {
+  return typeof window !== "undefined" ? window : null;
+}
+
+const remInPx = (rem: number) =>
+  rem * parseFloat(getComputedStyle(document.body).fontSize);
+
+export default function Welcome() {
+  const [scroll, setScroll] = createSignal(getWindow()?.scrollY ?? 0);
+  const window = getWindow();
+  if (!window) return null;
+
+  const listener = () => {
+    setScroll(getWindow()?.scrollY ?? 0);
+  };
+
+  getWindow()?.addEventListener("scroll", listener);
+  onCleanup(() => getWindow()?.removeEventListener("scroll", listener));
+
+  function animate(
+    from: number,
+    to: number,
+    start: number,
+    end: number,
+    t: number
+  ) {
+    if (t <= start) return from;
+    if (t >= end) return to;
+    return from + ((to - from) * (t - start)) / (end - start);
+  }
+
+  const scrollPercentage = createMemo(
+    () => scroll() / (getWindow()?.innerHeight ?? 1)
+  );
+
+  const headingSize = createMemo(() =>
+    animate(6, 3, 0.1, 0.3, scrollPercentage())
+  );
+
+  return (
+    <>
+      {scroll() > (window.innerHeight - remInPx(3)) / 2 + remInPx(0.5) && (
+        <div class="bg-neutral-900 z-10 text-yellow-400 text-center font-black w-full fixed leading-none top-0 py-2 text-[3rem] border-b border-neutral-800 border-opacity-100 transition-opacity">
+          Brendan
+        </div>
+      )}
+      <div class="text-center font-black leading-none">
+        <div class="h-32 flex flex-col justify-end">
+          <h1
+            class="text-white"
+            style={{
+              "font-size": `${headingSize()}rem`,
+            }}
+          >
+            {"Hey, I'm "}
+          </h1>
+        </div>
+        <div
+          class="text-yellow-400 sticky top-2"
+          style={{ "font-size": `${headingSize()}rem` }}
+        >
+          Brendan
+        </div>
+        <div class="h-32 pt-8">
+          <h2 class="text-white text-4xl font-black">
+            A <span class="text-green-600">software developer</span> from <br />{" "}
+            Western Australia
+          </h2>
+        </div>
+      </div>
+    </>
+  );
+}
