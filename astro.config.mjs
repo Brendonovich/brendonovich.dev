b5c30f3971f6e9f455c1874aecce1c5f6594fddb
Brendan Allan <brendonovich@outlook.com>
2022-11-27 06:16:11 +0800

    actually deployable

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
-import solid from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solid(), tailwind(), image()]
+  integrations: [tailwind(), image()]
 });
