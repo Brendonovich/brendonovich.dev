7a606d51761a38189944cf2777a732996acb0932
Brendan Allan <brendonovich@outlook.com>
2023-01-01 21:42:11 +0800

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,25 @@
 import { defineConfig } from 'astro/config';
 import image from "@astrojs/image";
 import prefetch from "@astrojs/prefetch";
-import unocss from "unocss/astro"
-
+import unocss from "unocss/astro";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypeExternalLinks from "rehype-external-links"
-
+import rehypeExternalLinks from "rehype-external-links";
 import { remarkReadingTime } from "./src/utils/reading-time.mjs";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.brendonovich.dev",
-  integrations: [unocss(), image(), prefetch()],
+  integrations: [unocss(), image(), prefetch(), sitemap()],
   markdown: {
     extendDefaultPlugins: true,
     rehypePlugins: [rehypeAutolinkHeadings, [rehypeExternalLinks, {
       rel: "noopener noreferrer",
       target: "_blank"
     }]],
-    remarkPlugins: [remarkReadingTime],
+    remarkPlugins: [remarkReadingTime]
   },
   experimental: {
     contentCollections: true
